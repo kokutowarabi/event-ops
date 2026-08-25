82f58630489d2d98bdb97451ceed141e77a0f807
kokutowarabi <shoyasan0523@gmail.com>
2026-08-25 23:12:08 +0900

    fix: restore edit pen hover animation

--- components/common/edit-icon.tsx
+++ components/common/edit-icon.tsx
@@ -10,7 +10,7 @@ export function EditIcon({ className, ...props }: ComponentProps<"span">) {
       {...props}
     >
       <span className="edit-icon-trail" />
-      <Pencil className="edit-icon-pen relative z-10 size-full" data-icon-motion="edit-pen" />
+      <Pencil className="edit-icon-pen relative z-10 size-full" data-icon-motion="shake" />
     </span>
   )
 }
