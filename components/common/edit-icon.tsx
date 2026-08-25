e019a5c9c3e51e2ca9c5412249d93651ef98d387
kokutowarabi <shoyasan0523@gmail.com>
2026-08-25 23:05:37 +0900

    fix: restore pencil icon and lengthen writing trail

--- components/common/edit-icon.tsx
+++ components/common/edit-icon.tsx
@@ -1,24 +1,16 @@
+import { Pencil } from "lucide-react"
 import type { ComponentProps } from "react"
 
-export function EditIcon({ className, ...props }: ComponentProps<"svg">) {
+export function EditIcon({ className, ...props }: ComponentProps<"span">) {
   return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <span
       className={className}
       data-icon-motion="edit"
       aria-hidden="true"
       {...props}
     >
-      <path className="edit-icon-trail" d="M3 21h10" />
-      <g className="edit-icon-pen" fill="none">
-        <path d="m4 16 10-10 4 4L8 20H4v-4Z" />
-        <path d="m13 7 4 4" />
-      </g>
-    </svg>
+      <span className="edit-icon-trail" />
+      <Pencil className="edit-icon-pen relative z-10 size-full" data-icon-motion="edit-pen" />
+    </span>
   )
 }
